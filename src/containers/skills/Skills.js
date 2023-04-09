3d60df69df471d48aeb2948a11040b519f906cff
Hazem <hennaceur@mun.ca>
2023-04-09 04:18:14 -0230

    fix contact section and center div

--- src/containers/skills/Skills.js
+++ src/containers/skills/Skills.js
@@ -29,36 +29,40 @@ export default function Skills() {
         </Fade>
         <Fade right duration={1000}>
           <div className="skills-text-div">
-            <h1
-              className={isDark ? "dark-mode skills-heading" : "skills-heading"}
-            >
-              {skillsSection.title}{" "}
-            </h1>
-            <p
-              className={
-                isDark
-                  ? "dark-mode subTitle skills-text-subtitle"
-                  : "subTitle skills-text-subtitle"
-              }
-            >
-              {skillsSection.subTitle}
-            </p>
-            <SoftwareSkill />
-            <div>
-              {skillsSection.skills.map((skills, i) => {
-                return (
-                  <p
-                    key={i}
-                    className={
-                      isDark
-                        ? "dark-mode subTitle skills-text"
-                        : "subTitle skills-text"
-                    }
-                  >
-                    {skills}
-                  </p>
-                );
-              })}
+            <div className="centered-element">
+              <h1
+                className={
+                  isDark ? "dark-mode skills-heading" : "skills-heading"
+                }
+              >
+                {skillsSection.title}{" "}
+              </h1>
+              <p
+                className={
+                  isDark
+                    ? "dark-mode subTitle skills-text-subtitle"
+                    : "subTitle skills-text-subtitle"
+                }
+              >
+                {skillsSection.subTitle}
+              </p>
+              <SoftwareSkill />
+              <div>
+                {skillsSection.skills.map((skills, i) => {
+                  return (
+                    <p
+                      key={i}
+                      className={
+                        isDark
+                          ? "dark-mode subTitle skills-text"
+                          : "subTitle skills-text"
+                      }
+                    >
+                      {skills}
+                    </p>
+                  );
+                })}
+              </div>
             </div>
           </div>
         </Fade>
